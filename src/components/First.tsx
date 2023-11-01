47c87bcad5cb6fa41fe951e71bcaddb091531695
lldan <safinildan@gmail.com>
2023-11-01 18:10:06 +0300

    адаптив для первой страницы

--- src/components/First.tsx
+++ src/components/First.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import '../scss/First.scss';
-import Image from 'next/image';
-import { motion } from 'framer-motion';
-import me from '../images/me.jpeg';
-import github from '../images/github.svg';
-import telegram from '../images/telegram.svg';
+import React from "react";
+import "../scss/First.scss";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import me from "../images/me.jpeg";
+import github from "../images/github.svg";
+import telegram from "../images/telegram.svg";
 export default function First() {
   return (
     <section className="first">
@@ -43,32 +43,34 @@ export default function First() {
       >
         Kazan
       </motion.div>
-      <motion.div
-        className="first__img"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, delay: 2 }}
-      >
-        <Image src={me} alt="Моя фотография" width={120} height={120} />
-      </motion.div>
-      <motion.a
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, delay: 2 }}
-        href="https://github.com/lld4n"
-        className="first__link github"
-      >
-        <Image src={github} alt="Github" width={60} height={60} />
-      </motion.a>
-      <motion.a
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, delay: 2 }}
-        href="https://t.me/lld4n"
-        className="first__link telegram"
-      >
-        <Image src={telegram} alt="Telegram" width={60} height={60} />
-      </motion.a>
+      <div className="first__block">
+        <motion.div
+          className="first__img"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 2 }}
+        >
+          <Image src={me} alt="Ильдан" width={120} height={120} />
+        </motion.div>
+        <motion.a
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 2 }}
+          href="https://github.com/lld4n"
+          className="first__link github"
+        >
+          <Image src={github} alt="Github" width={60} height={60} />
+        </motion.a>
+        <motion.a
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 2 }}
+          href="https://t.me/lld4n"
+          className="first__link telegram"
+        >
+          <Image src={telegram} alt="Telegram" width={60} height={60} />
+        </motion.a>
+      </div>
     </section>
   );
 }
